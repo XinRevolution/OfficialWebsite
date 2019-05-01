a9f65c1380f55880a7c6696d9c0eae5ee5528347
mike <tmal0909@gmail.com>
2019-05-02 00:42:42 +0800

    [2019/05/02] 專案建立

---
+++ OfficialWebsite/Controllers/IssueController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using OfficialWebsite.Models.Constant;
+
+namespace OfficialWebsite.Controllers
+{
+    public class IssueController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ OfficialWebsite/Models/Constant/TransitionType.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OfficialWebsite.Models.Constant
+{
+    public class TransitionType
+    {
+        public static string VERTICAL { get; } = "transition-vertical";
+
+        public static string HORIZONTAL { get; } = "transition-horizontal";
+
+        public static string EXPAND { get; } = "transition-expand";
+
+        public static string NONE { get; } = "transition-none";
+    }
+}
