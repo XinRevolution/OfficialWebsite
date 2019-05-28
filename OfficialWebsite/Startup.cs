1363159ce8710346ca169ff8c01697dbbf9879af
mike.chen <tmal0909@gmail.com>
2019-05-28 22:11:24 +0800

    刪除 Privacy & Cookie cshtml

--- OfficialWebsite/Startup.cs
+++ OfficialWebsite/Startup.cs
@@ -38,16 +38,7 @@ namespace OfficialWebsite
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-            else
-            {
-                app.UseExceptionHandler("/Home/Error");
-                app.UseHsts();
-            }
-
+            app.UseHsts();
             app.UseHttpsRedirection();
             app.UseStaticFiles();
             app.UseCookiePolicy();
